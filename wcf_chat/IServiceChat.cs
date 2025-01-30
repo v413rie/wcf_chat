7c419472ade4eb5d052f49a82fe15417b71d26bf
Валерия Тимашева <valeria397@outlook.com>
2025-01-30 23:49:21 +0300

    Добавьте файлы проекта.

---
+++ wcf_chat/IServiceChat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    
+    [ServiceContract(CallbackContract =typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg, int id);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallBack(string msg);
+    }
+}
